520f9989008e47079cec7f1967b1619ec556aeb4
Alex Ferreira <aferreira91@gmail.com>
2022-08-17 08:49:32 -0300

    iniciando projeto spring boot

---
+++ src/main/java/br/edu/uni7/tecnicas/controller/HomeController.java
@@ -0,0 +1,14 @@
+package br.edu.uni7.tecnicas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index.html";
+    }
+
+}
